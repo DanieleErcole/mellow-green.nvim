17d993941fa3e76a7e248789964d7adbce75da2b
DanieleErcole <danieleercole14@protonmail.com>
2026-09-07 19:50:03 +0200

    more green-oriented theme

--- lua/mellow/colors.lua
+++ lua/mellow/colors.lua
@@ -1,76 +1,76 @@
 local M = {}
 
 M.dark = {
-  -- Basic
-  bg = "#161617",
-  fg = "#c9c7cd",
-  bg_dark = "#131314",
-  -- Normal
-  black = "#27272a",
-  red = "#f5a191",
-  green = "#90b99f",
-  yellow = "#e6b99d",
-  blue = "#aca1cf",
-  magenta = "#e29eca",
-  cyan = "#ea83a5",
-  white = "#c1c0d4",
-  -- Bright
-  bright_black = "#353539",
-  bright_red = "#ffae9f",
-  bright_green = "#9dc6ac",
-  bright_yellow = "#f0c5a9",
-  bright_blue = "#b9aeda",
-  bright_magenta = "#ecaad6",
-  bright_cyan = "#f591b2",
-  bright_white = "#cac9dd",
-  -- Grays
-  gray00 = "#18181a",
-  gray01 = "#1b1b1d",
-  gray02 = "#2a2a2d",
-  gray03 = "#3e3e43",
-  gray04 = "#57575f",
-  gray05 = "#757581",
-  gray06 = "#9998a8",
-  gray07 = "#c1c0d4",
-  -- Special
-  none = "NONE",
+    -- Basic
+    bg = "#161617",
+    fg = "#c9c7cd",
+    bg_dark = "#131314",
+    -- Normal
+    black = "#27272a",
+    red = "#f5a191",
+    green = "#83b98f",
+    yellow = "#e6b99d",
+    blue = "#86b8b0",
+    magenta = "#e29eca",
+    cyan = "#aca1cf",
+    white = "#c1c0d4",
+    -- Bright
+    bright_black = "#353539",
+    bright_red = "#ffae9f",
+    bright_green = "#9dcc9f",
+    bright_yellow = "#f0c5a9",
+    bright_blue = "#9bc9c0",
+    bright_magenta = "#ecaad6",
+    bright_cyan = "#b9aeda",
+    bright_white = "#cac9dd",
+    -- Grays
+    gray00 = "#18181a",
+    gray01 = "#1b1b1d",
+    gray02 = "#2a2a2d",
+    gray03 = "#3e3e43",
+    gray04 = "#57575f",
+    gray05 = "#757581",
+    gray06 = "#9998a8",
+    gray07 = "#c1c0d4",
+    -- Special
+    none = "NONE",
 }
 M.light = {
-  -- Basic
-  bg = "#f7f7f7",
-  fg = "#4d4b65",
-  bg_dark = "#f9f9f9",
-  -- Normal
-  black = "#f4f4f4",
-  red = "#e05439",
-  green = "#598c15",
-  -- yellow = "#e08200",
-  yellow = "#c65c00",
-  blue = "#174c97",
-  magenta = "#bf386e",
-  cyan = "#d82b65",
-  white = "#5a5a5e",
-  -- Bright
-  bright_black = "#fafafa",
-  bright_red = "#ff684b",
-  bright_green = "#6fa32b",
-  -- bright_yellow = "#f2a50a",
-  bright_yellow = "#f27306",
-  bright_blue = "#215fac",
-  bright_magenta = "#d55488",
-  bright_cyan = "#f73e7c",
-  bright_white = "#6f6f74",
-  -- Grays
-  gray00 = "#f7f7f7",
-  gray01 = "#e6e6e7",
-  gray02 = "#d3d3d4",
-  gray03 = "#c1c1c2",
-  gray04 = "#afafb1",
-  gray05 = "#9d9da0",
-  gray06 = "#8c8c8f",
-  gray07 = "#7b7b7e",
-  -- Special
-  none = "NONE",
+    -- Basic
+    bg = "#f7f7f7",
+    fg = "#4d4b65",
+    bg_dark = "#f9f9f9",
+    -- Normal
+    black = "#f4f4f4",
+    red = "#e05439",
+    green = "#598c15",
+    -- yellow = "#e08200",
+    yellow = "#c65c00",
+    blue = "#174c97",
+    magenta = "#bf386e",
+    cyan = "#d82b65",
+    white = "#5a5a5e",
+    -- Bright
+    bright_black = "#fafafa",
+    bright_red = "#ff684b",
+    bright_green = "#6fa32b",
+    -- bright_yellow = "#f2a50a",
+    bright_yellow = "#f27306",
+    bright_blue = "#215fac",
+    bright_magenta = "#d55488",
+    bright_cyan = "#f73e7c",
+    bright_white = "#6f6f74",
+    -- Grays
+    gray00 = "#f7f7f7",
+    gray01 = "#e6e6e7",
+    gray02 = "#d3d3d4",
+    gray03 = "#c1c1c2",
+    gray04 = "#afafb1",
+    gray05 = "#9d9da0",
+    gray06 = "#8c8c8f",
+    gray07 = "#7b7b7e",
+    -- Special
+    none = "NONE",
 }
 
 return M
